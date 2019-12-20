877b544b8c71943e5f5c272a90645222b1ce1531
Dhruv Chitkara <dhruvchitkara27@gmail.com>
2019-12-20 16:57:40 +0530

    Handling user input with form

---
+++ image-searcher/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import App from './App';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.term}
+                            onChange={ e => this.setState({ term: e.target.value.toUpperCase() })} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
